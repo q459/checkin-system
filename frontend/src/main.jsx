18e5dcff64a896ff9111a4a616f8b9ade4d672cb
q459 <laifangjie3@gmail.com>
2026-08-06 13:08:14 +0800

    Fix main.jsx imports

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,18 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import { GoogleOAuthProvider } from '@react-oauth/google'
 import App from './App.jsx'
-
-// 請確保這裡填入你的 Google OAuth Client ID
-const GOOGLE_CLIENT_ID = "979046836859-5tnfvuk14ll953i5ed30htij8sjs213l.apps.googleusercontent.com";
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </GoogleOAuthProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 )
